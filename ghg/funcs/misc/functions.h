d9c23c83655acf573fb563e75c5c6748058f25ad
BSuzinator <brandon.suzelis@gmail.com>
2021-04-02 00:58:18 -0400

    added engineOff and roadCheck

--- ghg/funcs/misc/functions.h
+++ ghg/funcs/misc/functions.h
@@ -2,6 +2,8 @@ class misc
 {
 	file = "ghg\funcs\misc";
 	class addMinesToZeus { postInit = 1; };
+	class engineOff { postInit = 1; };
 	class fileExists {};
 	class reversePlanes { postInit = 1; };
+	class roadCheck { postInit = 1; };
 };
