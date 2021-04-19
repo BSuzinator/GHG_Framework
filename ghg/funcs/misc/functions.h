44dc58abc346ac10027c835a58f65e8e3d6575bc
BSuzinator <brandon.suzelis@gmail.com>
2021-04-18 22:02:14 -0400

    add fuel level check for engineers

--- ghg/funcs/misc/functions.h
+++ ghg/funcs/misc/functions.h
@@ -2,6 +2,7 @@ class misc
 {
 	file = "ghg\funcs\misc";
 	class addMinesToZeus { postInit = 1; };
+	class checkFuelLevel { postInit = 1; };
 	class engineOff { postInit = 1; };
 	class fileExists {};
 	class reversePlanes { postInit = 1; };
