cbfddd9c266abda2d1aa1b2d75d67ba4e5992eb8
Samuel Deutsch <sam.e.deutsch@gmail.com>
2021-09-17 22:02:30 -0700

    Added new framework

---
+++ GHG_Framework.VR/ghg/briefing/indfor.hpp
@@ -0,0 +1,17 @@
+class Entry1
+{
+	title="Overview";
+    text="<font size='24' face='TahomaB'>Overview</font><br />Last week one of the officers from SOCOM was taken hostage from a routine convoy in Takistan. Earlier this morning we got intel that he was being held here in Zargabad, and a local insurgent cell wants to negotiate for his release. These insurgents have been a thorn in the local authorities collective side, and they have also contacted us to aid in ousting them. The Russians have an Airbase in the region, however they have made a point of staying quiet throughout all this. We suspect they are supplying the insurgents, but no evidence can be found, so no action can be taken.";
+};
+
+class Entry2
+{
+	title="Commander's Intent";
+    text="<font size='24' face='TahomaB'>Commander's Intent</font><br />We need to rescue the Officer by any means necessary. There have been rumors of where the insurgent HQ is, however we are not here to kick a hornets' nest unless we have to. We lost the last drone we flew over the area from interference, so be on the lookout for a Jammer.<br /><br /><font size='18' face='TahomaB'>Task List</font><br />-Find and Rescue the Officer<br />-Destroy all Jammers<br />-RTB<br /><br /><font size='18' face='TahomaB'>Friendly Assets</font><br />-HUMVEEs for troop transport<br />-M113 for LOGI Support<br />-1x Medical Crate<br />-2x Fireteam Crates<br />-1x LOGI Crate";
+};
+
+class Entry3
+{
+    title="Expected Contacts";
+    text="<font size='24' face='TahomaB'>Expected Contacts</font><br />-Company+ Insurgents<br />-IEDs/Mines";
+};
