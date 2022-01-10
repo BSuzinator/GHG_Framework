ab948cae3740556774b6f5c02a4749bf1ce61f2d
BSuzinator <brandon.suzelis@gmail.com>
2022-01-10 03:50:03 -0500

    add addon for statistic tracking / leaderboards

---
+++ addons/statistics/CfgVehicles.hpp
@@ -0,0 +1,29 @@
+class CfgVehicles
+{
+    class Bag_Base;
+    class GHG_RuggedNova_blk: Bag_Base
+    {
+        author = "QuantX";
+        scope = 2;
+        displayName = "Rolm 1602 Computer (Black)";
+        mass = 265;
+        maximumLoad = 0;
+
+        model = "\x\ghg\addons\hacking\models\ruggednova\ruggednova";
+        picture = "\x\ghg\addons\hacking\models\ruggednova\ruggednova_icon.paa";
+
+        hiddenSelectionsTextures[] = {"a3\weapons_f\ammoboxes\bags\data\backpack_compact_blk_co.paa"};
+    };
+    
+    class GHG_RuggedNova_khk: GHG_RuggedNova_blk
+    {
+        displayName = "Rolm 1602 Computer (Khaki)";
+        hiddenSelectionsTextures[] = {"\A3\weapons_f\ammoboxes\bags\data\backpack_compact_khk_co.paa"};
+    };
+    
+    class GHG_RuggedNova_cbr: GHG_RuggedNova_blk
+    {
+        displayName = "Rolm 1602 Computer (Coyote)";
+        hiddenSelectionsTextures[] = {"\A3\weapons_f\ammoboxes\bags\data\backpack_compact_cbr_co.paa"};
+    };
+};
---
+++ addons/statistics/script_component.hpp
@@ -0,0 +1,4 @@
+#define COMPONENT statistics
+#define COMPONENT_BEAUTIFIED Statistics
+#include "\x\ghg\addons\main\script_mod.hpp"
+#include "\x\ghg\addons\main\script_macros.hpp"
