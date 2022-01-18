6412af52c21622ba81a5ce41fea749f3cb97b67e
Samuel Deutsch <sam.e.deutsch@gmail.com>
2022-01-17 16:33:01 -0800

    Reduced respawn time (worst case is 39 minutes)

--- addons/respawn/script_component.hpp
+++ addons/respawn/script_component.hpp
@@ -3,7 +3,7 @@
 #include "\x\ghg\addons\main\script_mod.hpp"
 #include "\x\ghg\addons\main\script_macros.hpp"
 
-#define PLAYERS_PER_SQUAD 7
-#define TIME_PER_PLAYER 5
+#define PLAYERS_PER_SQUAD 6
+#define TIME_PER_PLAYER 3
 #define MAX_WAIT_TIME (PLAYERS_PER_SQUAD * TIME_PER_PLAYER)
 #define DUMMY_WAIT_TIME 100000
