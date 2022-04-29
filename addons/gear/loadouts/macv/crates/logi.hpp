333d8692f80c2c23b4d5b4f6f9a87e13a628352b
BSuzinator <brandon.suzelis@gmail.com>
2022-04-29 19:59:15 -0400

    macv crates updated

---
+++ addons/gear/loadouts/macv/crates/logi.hpp
@@ -0,0 +1,28 @@
+class ghg_crate_macv_logi : ghg_crate_base
+{
+    displayname = "Logistics";
+    cargosize = 2;
+    
+    class items : items {
+        ACE_elasticBandage = 12;
+        ACE_packingBandage = 12;
+        vn_m16_20_mag = 32;
+        vn_m61_grenade_mag = 4;
+        vn_m18_white_mag = 6;
+        vn_m72 = 1;
+        vn_m72_mag = 1;
+		ACE_wirecutter = 2;
+		ACE_DefusalKit = 2;
+		ToolKit = 2;
+        vn_40mm_m381_he_mag = 8;
+		vn_40mm_m583_flare_w_mag = 4;
+		vn_40mm_m661_flare_g_mag = 2;
+		vn_40mm_m662_flare_r_mag = 2;
+		vn_40mm_m682_smoke_r_mag = 2;
+		vn_40mm_m715_smoke_g_mag = 2;
+		vn_40mm_m680_smoke_w_mag = 4;
+		ACE_EarPlugs = 8;
+		diw_armor_plates_main_plate = 8;
+		B_Kitbag_cbr = 2;
+    };
+};
