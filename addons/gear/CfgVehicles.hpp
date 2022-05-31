0841210cd600633a4595fff528f9c7cfa266414f
BSuzinator <brandon.suzelis@gmail.com>
2022-05-31 17:52:08 -0400

    add GHG flag and banner

--- addons/gear/CfgVehicles.hpp
+++ addons/gear/CfgVehicles.hpp
@@ -117,4 +117,24 @@ class CfgVehicles
     GHG_SIDE_CLASS(b)
     GHG_SIDE_CLASS(o)
     GHG_SIDE_CLASS(i)
+	
+	class FlagCarrier;
+	class ghg_Flag_GHG : FlagCarrier
+    {
+        scope=2;
+        author = "GHG Team";
+		displayName = "Flag (GHG)";
+        class EventHandlers
+        {
+            init="(_this select 0) setFlagTexture '\x\ghg\addons\main\ui\ghgFlag.paa'";
+        };
+    };
+	class Banner_01_base_F;
+	class ghg_Banner_GHG : Banner_01_base_F
+	{
+		scope=2;
+        author = "GHG Team";
+		displayName = "Banner (GHG)";
+		hiddenSelectionsTextures[] = {"\x\ghg\addons\main\ui\ghgFlag.paa"};
+	};
 };
