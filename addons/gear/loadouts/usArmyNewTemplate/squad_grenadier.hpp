9e036d440e0d44654a2683aa21271f9beb16a1b0
BSuzinator <brandon.suzelis@gmail.com>
2022-06-02 22:04:00 -0400

    Update squad_grenadier.hpp

--- addons/gear/loadouts/usArmyNewTemplate/squad_grenadier.hpp
+++ addons/gear/loadouts/usArmyNewTemplate/squad_grenadier.hpp
@@ -5,7 +5,6 @@ class squad_grenedier : squad_sl
 {
 	class Vest : Vest
     {
-        classname[] = { "rhsusf_iotv_ocp_Squadleader", "rhsusf_iotv_ocp_Squadleader", "rhsusf_iotv_ocp_Squadleader", "rhsusf_iotv_ucp_Squadleader", "rhsusf_iotv_ucp_Squadleader" };
 		ACE_MapTools = 0;
 		ACE_EntrenchingTool = 1;
 		ACRE_PRC152 = 0;
