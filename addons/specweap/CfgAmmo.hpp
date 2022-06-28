776dca0884cdd0d60085daa5604abbb6c03ff244
Samuel Deutsch <sam.e.deutsch@gmail.com>
2022-06-27 17:45:29 -0700

    Used tighter distribution for Beehive

--- addons/specweap/CfgAmmo.hpp
+++ addons/specweap/CfgAmmo.hpp
@@ -64,7 +64,7 @@ class CfgAmmo
     class ghg_shell_105mm_m546_beehive_ammo : vn_sh_105mm_base_he_ammo
     {
         submunitionAmmo = "B_12Gauge_Pellets_Submunition_Deploy";
-        submunitionConeType[] = {"poissondisc", 800};
+        submunitionConeType[] = {"poissondisccenter", 800};
         submunitionConeAngle = 18;
         submunitionDirectionType = "SubmunitionModelDirection";
         triggerTime = 0.1; // Init Speed is 810m/s so this is about 81m out
