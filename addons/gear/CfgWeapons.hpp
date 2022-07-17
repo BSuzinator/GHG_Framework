87a29b05faf1ea0d2fbb276ed843025d5ba22963
Samuel Deutsch <sam.e.deutsch@gmail.com>
2022-07-17 04:30:22 -0700

    Add flashlights to all SOG rifles

--- addons/gear/CfgWeapons.hpp
+++ addons/gear/CfgWeapons.hpp
@@ -66,6 +66,35 @@ class CfgWeapons
         };
     };
     
+    // Add flashlights to all SOG primary weapons
+    class vn_rifle : Rifle_Base_F {
+        class FlashLight {
+            ambient[] = {9,7.5,6};
+            color[] = {180,150,120};
+            coneFadeCoef = 55;
+            dayLight = 1;
+            direction = "konec hlavne";
+            flareMaxDistance = "100.0f";
+            flareSize = 1.4;
+            innerAngle = 20;
+            intensity = 50;
+            outerAngle = 120;
+            position = "usti hlavne";
+            scale[] = {0};
+            size = 1;
+            useFlare = 1;
+            
+            class Attenuation {
+                constant = 0;
+                hardLimitEnd = 30;
+                hardLimitStart = 20;
+                linear = 0;
+                quadratic = 1;
+                start = 0.5;
+            };
+        };
+    };
+    
     class vn_rifle762;
     class vn_m14 : vn_rifle762 {
         modes[] = {
