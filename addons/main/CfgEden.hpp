4a8f2f36c40b60c380d203c421af35b96861d08b
BSuzinator <brandon.suzelis@gmail.com>
2022-07-24 14:53:35 -0400

    Create CfgEden.hpp

---
+++ addons/main/CfgEden.hpp
@@ -0,0 +1,10 @@
+class Cfg3DEN {
+    class Object {
+        class AttributeCategories {
+            class ghg_attributes {
+                displayName = "GHG Options";
+                collapsed = 1;
+                class Attributes {};
+            };
+        };
+    };
