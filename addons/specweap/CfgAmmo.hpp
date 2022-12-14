c1d3deb393003119e96978dc47890ba524ee1c8f
BSuzinator <brandon.suzelis@gmail.com>
2022-12-13 23:29:11 -0500

    fix surprise

--- addons/specweap/CfgAmmo.hpp
+++ addons/specweap/CfgAmmo.hpp
@@ -77,10 +77,12 @@ class CfgAmmo
 	class rhs_ammo_Hydra_M229;
 	class ghg_ammo_Hydra_STUN : rhs_ammo_Hydra_M229 {
 		hit = 1;
-		indirectHit = 0;
-		indirectHitRange = 0;
+		indirectHit = 0.3;
+		indirectHitRange = 0.1;
 		ExplosionEffects = "RHSUSF_flashbang_20";
-		explosive = 0;
+		explosionTime = 0.5;
+		//explosive = 0;
 		warheadName = "STUN";
+		cost = 10;
 	};
 };
