9c0fc5e5274424d448b002508d08ee1214749822
BSuzinator <brandon.suzelis@gmail.com>
2022-12-28 22:54:09 -0500

    hopefully fix servicepoints in zeus

--- addons/gear/config.cpp
+++ addons/gear/config.cpp
@@ -14,7 +14,7 @@ class CfgPatches {
         // When any of the addons is missing, pop-up warning will appear when launching the game.
         requiredAddons[] = { "ghg_main", "rhs_c_weapons", "rhsusf_c_weapons", "rhsgref_c_weapons", "weapons_f_vietnam", "weapons_f_vietnam_02", "weapons_f_vietnam_03" };
         // List of objects (CfgVehicles classes) contained in the addon. Important also for Zeus content (units and groups) unlocking.
-        units[] = {"ghg_servicePoint_air","ghg_servicePoint_psp","ghg_servicePoint_air_invis","ghg_servicePoint_land","ghg_servicePoint_land_invis"};
+        units[] =  { "ghg_servicePoint_air", "ghg_servicePoint_air_psp", "ghg_servicePoint_air_invis", "ghg_servicePoint_land", "ghg_servicePoint_land_invis" };
         // List of weapons (CfgWeapons classes) contained in the addon.
         weapons[] = {};
     };
