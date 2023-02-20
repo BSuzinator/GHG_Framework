78019ccec12ca69ec44fdd498a8645be3672ea68
BSuzinator <brandon.suzelis@gmail.com>
2023-02-19 21:47:52 -0500

    fix bokoHaram not being added

--- addons/gear/loadouts/loadouts.hpp
+++ addons/gear/loadouts/loadouts.hpp
@@ -2,6 +2,7 @@ class Loadouts
 {
 	#include "baf\loadouts.hpp"
 	#include "bafCW\loadouts.hpp"
+	#include "bokoHaram\loadouts.hpp"
 	#include "cbrn\loadouts.hpp"
 	#include "gendarmerieSwat\loadouts.hpp"
 	#include "macv\loadouts.hpp"
