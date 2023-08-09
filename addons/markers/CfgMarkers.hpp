63bc10abfa56d5dcba12f177cc04f6b25ced72de
BSuzinator <brandon.suzelis@gmail.com>
2023-08-09 15:12:24 -0400

    fix icon path

--- addons/markers/CfgMarkers.hpp
+++ addons/markers/CfgMarkers.hpp
@@ -2,7 +2,7 @@ class CfgMarkers {
 	class Flag;
 	class GHG_Mkr : Flag {
 		color[] = {0.26,0.26,0.26,1};
-		icon = "ghg\main\ui\logo.paa";
+		icon = "x\ghg\main\ui\logo.paa";
 		name = "GHG_Mkr";
 		shadow = 1;
 		size = 32;
@@ -11,6 +11,6 @@ class CfgMarkers {
 		scope = 2;
 		markerClass = "Flags";
 		name = "Ghost Hawk Gaming";
-		texture = "ghg\main\ui\logo.paa";
+		texture = "x\ghg\main\ui\logo.paa";
 	};
 };
