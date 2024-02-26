9e88169abc590917c410605be0e7cda6abda3bcd
BSuzinator <brandon.suzelis@gmail.com>
2024-02-25 20:02:21 -0500

    Revert "Fixed statics to remove rhs dependencies"
    This reverts commit a434edbf93f6efd7b058f919e02d2a68d47a1e25.

--- addons/gear/loadouts/common_crates/ru_statics.hpp
+++ addons/gear/loadouts/common_crates/ru_statics.hpp
@@ -3,7 +3,7 @@ class ghg_crate_ru_gmgAmmo : ghg_crate_base
     displayname = "GMG Ammo";
     
     class items : items {
-        ace_compat_sog_mk18_48 = 4;
+        ace_compat_rhs_afrf3_mag_VOG30_30 = 4;
     };
 };
 class ghg_crate_ru_gmgGun : ghg_crate_base
@@ -12,7 +12,7 @@ class ghg_crate_ru_gmgGun : ghg_crate_base
     
     class items : items {
         ace_csw_sag30CarryTripod = 1;
-		ace_compat_sog_mk18_carry = 1;
+		ace_compat_rhs_afrf3_ags30_carry = 1;
     };
 };
 class ghg_crate_ru_hatAmmo : ghg_crate_base
@@ -20,7 +20,7 @@ class ghg_crate_ru_hatAmmo : ghg_crate_base
     displayname = "HAT Ammo";
     
     class items : items {
-        ace_compat_sog_tow_missile = 8;
+        ace_compat_rhs_afrf3_mag_9M131M = 8;
     };
 };
 class ghg_crate_ru_hatTube : ghg_crate_base
@@ -28,8 +28,7 @@ class ghg_crate_ru_hatTube : ghg_crate_base
     displayname = "HAT Tube";
     
     class items : items {
-		ace_csw_m220CarryTripod = 1;
-        ace_compat_sog_tow_carry = 1;
+        ace_compat_rhs_afrf3_mag_9M131M = 8;
     };
 };
 class ghg_crate_ru_hmgAmmo : ghg_crate_base
@@ -37,7 +36,7 @@ class ghg_crate_ru_hmgAmmo : ghg_crate_base
     displayname = "HMG Ammo";
     
     class items : items {
-        ace_compat_sog_dshkm_50 = 12;
+        ace_csw_50Rnd_127x108_mag = 12;
     };
 };
 class ghg_crate_ru_hmgGun : ghg_crate_base
@@ -47,6 +46,6 @@ class ghg_crate_ru_hmgGun : ghg_crate_base
     class items : items {
         ace_csw_kordCarryTripod = 1;
 		ace_csw_kordCarryTripodLow = 1;
-		ace_compat_sog_dshkm_carry = 1;
+		ace_compat_rhs_gref3_dshkm_carry = 1;
     };
 };
