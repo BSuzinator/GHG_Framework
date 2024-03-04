3dcb86fb41ed9f9b53994a1baa9591c9bfb24f45
BSuzinator <brandon.suzelis@gmail.com>
2024-03-03 19:34:37 -0500

    fix rangefinder having ammo

--- addons/gear/loadouts/usArmy_RHS/rcn_gunner.hpp
+++ addons/gear/loadouts/usArmy_RHS/rcn_gunner.hpp
@@ -45,7 +45,7 @@ class rcn_gunner : loadout_base
     headgear[] = { "rhsusf_ach_bare_wood", "rhsusf_ach_helmet_ocp", "rhsusf_ach_helmet_ocp", "rhsusf_ach_helmet_ucp", "rhsusf_ach_helmet_ucp" };
     facewear = "rhsusf_oakley_goggles_clr";
 	
-	class Binoculars : Weapon_1 // Yes, the binoculars are technically a weapon
+	class Binoculars : Binoculars // Yes, the binoculars are technically a weapon
     {
         classname = "ACE_Yardage450";
 
