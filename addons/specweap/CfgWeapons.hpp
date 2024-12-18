ca65c8e4061c17f7aae9f2a0d7dfa6a74998c028
BSuzinator <brandon.suzelis@gmail.com>
2024-12-18 18:40:18 -0500

    Update CfgWeapons.hpp

--- addons/specweap/CfgWeapons.hpp
+++ addons/specweap/CfgWeapons.hpp
@@ -28,10 +28,15 @@ class CfgWeapons
         magazines[] += {
             "ghg_cannon_m101_mag_beehive_x8"
         };
-    };
+    };	
 	
-	class rhs_weap_FFARLauncher;
-	class rhs_weap_FFARLauncher_M229 : rhs_weap_FFARLauncher {
-		magazines[] += {"rhs_mag_M229_stun_7","rhs_mag_M229_stun_19"};
+	class Rifle_Base_F;
+	class arifle_MX_Base_F : Rifle_Base_F {
+		magazineWell[] = {"MX_65x39","CBA_65x39_MX"};
 	};
+	class arifle_MX_SW_F : arifle_MX_Base_F {
+		magazineWell[] = {"MX_65x39","MX_65x39_Large","CBA_65x39_MX","CBA_65x39_MX_XL"};
+	};
+	
+	
 };
