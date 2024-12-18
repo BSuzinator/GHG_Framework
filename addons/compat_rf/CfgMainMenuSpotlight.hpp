1306ff71448d5e1d136e1b18f60d9aae6b54c784
BSuzinator <brandon.suzelis@gmail.com>
2024-12-18 18:40:12 -0500

    Reaction Forces Compat

---
+++ addons/compat_rf/CfgMainMenuSpotlight.hpp
@@ -0,0 +1,6 @@
+class CfgMainMenuSpotlight
+{
+	delete Scenario_AirControl_RF;
+    delete Scenario_FireSeason_RF;
+    delete Scenario_TrainingDay_RF;
+};
---
+++ addons/compat_rf/script_component.hpp
@@ -0,0 +1,4 @@
+#define COMPONENT ghg_compat_rf
+#define COMPONENT_BEAUTIFIED GHG_Compat_RF
+#include "\x\ghg\addons\main\script_mod.hpp"
+#include "\x\ghg\addons\main\script_macros.hpp"
